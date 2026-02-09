d4a665d9af06a1f8634e2a8e4b9ddb616ecaf89b
vldmrnvs <vldmrnvs@gmail.com>
2026-02-09 23:17:47 +0100

    Add files via upload

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import './styles/glass.css'
-import 'geist/style.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
